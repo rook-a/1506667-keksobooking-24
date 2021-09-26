5862ce108c873485fdccce551f9780734b47553a
rook-a <zhernovkov_konst@mail.ru>
2021-09-26 18:28:03 +0300

    доработал проверки в функциях

--- js/main.js
+++ js/main.js
@@ -2,24 +2,21 @@ const getRandomNumber = function (from, to) {
   from = Math.ceil(from); //MDN
   to = Math.floor(to); //MDN
 
-  if (from === to || to < from || to < 0) {
-    return false;
-  }
-
-  return Math.round(Math.random() * (to - from) + from); //MDN
+  return (from >= 0 && to > from) ? Math.round(Math.random() * (to - from) + from) : false;
 };
 
 const getRandomNumberDot = function (from, to, count) {
-  if (from === to || to < from || to < 0) {
-    return false;
-  }
 
   const randomNumber = Math.random() * (to - from) + from;
 
-  return randomNumber.toFixed(count);
+  return (from >= 0 && to > from && count > 0) ? Number(randomNumber.toFixed(count)) : false;
 };
 
 getRandomNumber();
 getRandomNumberDot();
 
-// строки 2, 3, 9 взяты тут https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+/* строки 2, 3, 5, 10 взяты тут https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  from = Math.ceil(from); //MDN
+  to = Math.floor(to)
+  Math.round(Math.random() * (to - from) + from)
+*/
