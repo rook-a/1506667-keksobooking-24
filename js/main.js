43af4f435533867a8ca3a4ce84fd719adfd5e91b
rook-a <zhernovkov_konst@mail.ru>
2021-10-03 23:47:21 +0300

    добавил функцию и исправил ошибки

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const AVATAR_COUNTS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const AVATAR_COUNTS = new Array(10).fill(null);
 
 const TYPE = [
   'palace',
@@ -8,12 +8,6 @@ const TYPE = [
 ];
 
 const CHECKIN = [
-  '12:00',
-  '13:00',
-  '14:00',
-];
-
-const CHECKOUT = [
   '12:00',
   '13:00',
   '14:00',
@@ -56,27 +50,44 @@ const getRandomArrayElement = function (elements) {
   return elements[getRandomNumber(0, elements.length - 1)];
 };
 
+const getRandomArrayString = function (arr) {
+  const strings = [];
+
+  for (let el = 0; el <= arr.length - 1; el++) {
+    if (getRandomNumber(0, 1) > 0) {
+      strings.push(arr[el]);
+    }
+  }
+
+  return strings;
+};
+
+//console.log(getRandomArrayString(FEATURES));
+
 const createAd = function (index) {
+  const locationLat = getRandomNumberDot(35.65000, 35.70000, 5);
+  const locationLng = getRandomNumberDot(139.70000, 139.80000, 5);
+
   return {
     author: {
       avatar: `img/avatars/user${addZero(index + 1)}.png`,
     },
     offer: {
       title: 'Сдаются комфортные апартаменты',
-      address: `${getRandomNumberDot(35.65000, 35.70000, 5)} ${getRandomNumberDot(139.70000, 139.80000, 5)}`,
+      address: `${locationLat}, ${locationLng}`,
       price: getRandomNumber(1000, 10000),
       type: getRandomArrayElement(TYPE),
       rooms: getRandomNumber(1, 5),
       guests: getRandomNumber(1, 10),
       checkin: getRandomArrayElement(CHECKIN),
-      checkout: getRandomArrayElement(CHECKOUT),
-      features: getRandomArrayElement(FEATURES),
+      checkout: getRandomArrayElement(CHECKIN),
+      features: getRandomArrayString(FEATURES),
       description: 'Просторные комнаты. Высокие потолки. Близко к океану. Бассейн и джакузи на территории.',
-      photos: getRandomArrayElement(PHOTOS),
+      photos: getRandomArrayString(PHOTOS),
     },
     location: {
-      lat: getRandomNumberDot(35.65000, 35.70000, 5),
-      lng: getRandomNumberDot(139.70000, 139.80000, 5),
+      lat: locationLat,
+      lng: locationLng,
     },
   };
 };
