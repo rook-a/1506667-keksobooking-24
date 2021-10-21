099d11808e6f34ab6ad2bc0092a7cfd916cb08aa
rook-a <zhernovkov_konst@mail.ru>
2021-10-21 22:39:35 +0300

    исправил ошибки. добавил синхранизацию полей timeIn и timeOut

--- js/form-validity.js
+++ js/form-validity.js
@@ -2,234 +2,211 @@ const adForm = document.querySelector('.ad-form');
 const adTitle = adForm.querySelector('#title');
 const adPrice = adForm.querySelector('#price');
 const adTypes = adForm.querySelector('#type');
-const adBtn = adForm.querySelector('.ad-form__submit');
 const adRooms = adForm.querySelector('#room-number');
 const adCapacity = adForm.querySelector('#capacity');
+const adCapacityOption = Array.from(adCapacity.children);
+const adTimeIn = adForm.querySelector('#timein');
+const adTimeOut = adForm.querySelector('#timeout');
 
 const MIN_TITLE_LENGTH = 30;
 const MAX_TITLE_LENGTH = 100;
 const MAX_PRICE_VALUE = 1000000;
 
-//error style
-const addErrorStyle = (isError, item) => {
-  const styleError = 'box-shadow: 0 0 2px 2px #FF0000;';
-
-  (isError) ? item.style = styleError : item.style = '';
+const PRICE_TO_TYPE = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000,
 };
 
-//disabled submit btn
-const disabledBtn = (isDisabled) => (isDisabled) ? adBtn.setAttribute('disabled', 'disabled') : adBtn.removeAttribute('disabled', 'disabled');
+const ROOMS_TO_CAPACITY = {
+  1: [1],
+  2: [1, 2],
+  3: [1, 2, 3],
+  100: [0],
+};
 
 //title
 adTitle.addEventListener('invalid', () => {
+  const valueLength = adTitle.value.length;
+
   if (adTitle.validity.valueMissing) {
+    adTitle.classList.add('ad-form__element-error');
     adTitle.setCustomValidity('Обязательное поле');
-    addErrorStyle(true, adTitle);
-    disabledBtn(true);
-  } else if (adTitle.value.length < MIN_TITLE_LENGTH || adTitle.value.length > MAX_TITLE_LENGTH) {
-    addErrorStyle(true, adTitle);
-    disabledBtn(true);
-  } else {
-    adTitle.setCustomValidity('');
-    addErrorStyle(false, adTitle);
+    return;
   }
+
+  if (valueLength < MIN_TITLE_LENGTH) {
+    adTitle.classList.add('ad-form__element-error');
+    adTitle.setCustomValidity(`Ещё ${MIN_TITLE_LENGTH - valueLength} симв.`);
+    return;
+  }
+
+  if (valueLength > MAX_TITLE_LENGTH) {
+    adTitle.classList.add('ad-form__element-error');
+    adTitle.setCustomValidity(`Удалите лишние ${valueLength - MAX_TITLE_LENGTH} симв.`);
+    return;
+  }
+  adTitle.classList.remove('ad-form__element-error');
+  adTitle.setCustomValidity('');
 });
 
 adTitle.addEventListener('input', () => {
-  const valueLength = adTitle.value.length;
-
-  if (valueLength < MIN_TITLE_LENGTH) {
-    adTitle.setCustomValidity(`Ещё ${MIN_TITLE_LENGTH - valueLength} симв.`);
-  } else if (valueLength > MAX_TITLE_LENGTH) {
-    adTitle.setCustomValidity(`Удалите лишние ${valueLength - MAX_TITLE_LENGTH} симв.`);
-  } else {
-    adTitle.setCustomValidity('');
-    addErrorStyle(false, adTitle);
-    disabledBtn(false);
-  }
-
   adTitle.reportValidity();
 });
 
 //price
 adPrice.addEventListener('invalid', () => {
   if (adPrice.validity.valueMissing) {
+    adPrice.classList.add('ad-form__element-error');
     adPrice.setCustomValidity('Обязательное поле');
-    addErrorStyle(true, adPrice);
-    disabledBtn(true);
-  } else {
-    adPrice.setCustomValidity('');
-    addErrorStyle(false, adPrice);
-    disabledBtn(false);
+    return;
   }
 
   if (adPrice.value > MAX_PRICE_VALUE) {
-    adPrice.setCustomValidity(`Слишком высокая цена за ночь. Уменьшите на ${adPrice.value - MAX_PRICE_VALUE} руб.`);
-    addErrorStyle(true, adPrice);
-    disabledBtn(true);
-  } else {
-    adPrice.setCustomValidity('');
-    addErrorStyle(false, adPrice);
-    disabledBtn(false);
+    adPrice.classList.add('ad-form__element-error');
+    adPrice.setCustomValidity(`Слишком высокая цена за ночь. Уменьшите цену на ${adPrice.value - MAX_PRICE_VALUE} руб.`);
+    return;
   }
+
+  if (adPrice.value < adPrice.min) {
+    adPrice.classList.add('ad-form__element-error');
+    adPrice.setCustomValidity(`Цена за ночь меньше допустимой. Увеличьте цену на ${adPrice.min - adPrice.value} руб.`);
+    return;
+  }
+
+  adPrice.setCustomValidity('');
+  adPrice.classList.remove('ad-form__element-error');
 });
 
 adPrice.addEventListener('input', () => {
+  /*
   const priceValue = adPrice.value;
 
   if (priceValue > MAX_PRICE_VALUE) {
-    //adPrice.setCustomValidity(`Слишком высокая цена за ночь. Уменьшите на ${priceValue - MAX_PRICE_VALUE} руб.`);
-    addErrorStyle(true, adPrice);
-    disabledBtn(true);
-  } else {
-    addErrorStyle(false, adPrice);
-    disabledBtn(false);
+    adPrice.classList.add('ad-form__element-error');
+    adPrice.setCustomValidity(`Слишком высокая цена за ночь. Уменьшите цену на ${priceValue - MAX_PRICE_VALUE} руб.`);
+    return;
   }
 
+  if (priceValue < adPrice.min) {
+    adPrice.classList.add('ad-form__element-error');
+    adPrice.setCustomValidity(`Цена за ночь меньше допустимой. Увеличьте цену на ${adPrice.min - priceValue} руб.`);
+    return;
+  }
+
+  adPrice.setCustomValidity('');
+  adPrice.classList.remove('ad-form__element-error');
+  */
   adPrice.reportValidity();
 });
 
 //type
 const onPriceChange = () => {
-
-  if (adTypes.value === 'bungalow') {
-    adPrice.placeholder = 0;
-    adPrice.setAttribute('min', '0');
-  }
-
-  if (adTypes.value === 'flat') {
-    adPrice.placeholder = 1000;
-    adPrice.setAttribute('min', '1000');
-  }
-
-  if (adTypes.value === 'hotel') {
-    adPrice.placeholder = 3000;
-    adPrice.setAttribute('min', '3000');
-  }
-
-  if (adTypes.value === 'house') {
-    adPrice.placeholder = 5000;
-    adPrice.setAttribute('min', '5000');
-  }
-
-  if (adTypes.value === 'palace') {
-    adPrice.placeholder = 10000;
-    adPrice.setAttribute('min', '10000');
-  }
+  adPrice.placeholder = PRICE_TO_TYPE[adTypes.value];
+  adPrice.setAttribute('min', PRICE_TO_TYPE[adTypes.value]);
 };
 
 adTypes.addEventListener('change', onPriceChange);
 
 //adRooms && adCapacity
-/*
-const disabledCapacity = () => {
-  switch (adRooms.value) {
-    case '1':
-      adCapacity[0].setAttribute('disabled', 'disabled');
-      adCapacity[1].setAttribute('disabled', 'disabled');
-      adCapacity[2].removeAttribute('disabled', 'disabled');
-      adCapacity[3].setAttribute('disabled', 'disabled');
-      break;
-    case '2':
-      adCapacity[0].setAttribute('disabled', 'disabled');
-      adCapacity[1].removeAttribute('disabled', 'disabled');
-      adCapacity[2].removeAttribute('disabled', 'disabled');
-      adCapacity[3].setAttribute('disabled', 'disabled');
-      break;
-    case '3':
-      adCapacity[0].removeAttribute('disabled', 'disabled');
-      adCapacity[1].removeAttribute('disabled', 'disabled');
-      adCapacity[2].removeAttribute('disabled', 'disabled');
-      adCapacity[3].setAttribute('disabled', 'disabled');
-      break;
-    case '100':
-      adCapacity[0].setAttribute('disabled', 'disabled');
-      adCapacity[1].setAttribute('disabled', 'disabled');
-      adCapacity[2].setAttribute('disabled', 'disabled');
-      adCapacity[3].removeAttribute('disabled', 'disabled');
-      break;
-    default:
-      adCapacity[0].removeAttribute('disabled', 'disabled');
-      adCapacity[1].removeAttribute('disabled', 'disabled');
-      adCapacity[2].removeAttribute('disabled', 'disabled');
-      adCapacity[3].removeAttribute('disabled', 'disabled');
-      break;
-  }
-};
-
-adRooms.addEventListener('change', disabledCapacity);
-*/
-
 const onElementInvalid = () => {
   if (adRooms.value >= adCapacity.value) {
     adRooms.setCustomValidity('');
-    addErrorStyle(false, adRooms);
-    addErrorStyle(false, adCapacity);
-    disabledBtn(false);
   } else {
     adRooms.setCustomValidity('Комнат не хватает!');
-    addErrorStyle(true, adRooms);
-    addErrorStyle(true, adCapacity);
-    disabledBtn(true);
   }
 
   if (adRooms.value < adCapacity.value) {
     adRooms.setCustomValidity('Нужно больше комнат!');
-    addErrorStyle(true, adRooms);
-    addErrorStyle(true, adCapacity);
-    disabledBtn(true);
   } else {
     adRooms.setCustomValidity('');
-    addErrorStyle(false, adRooms);
-    addErrorStyle(false, adCapacity);
-    disabledBtn(false);
   }
 };
 
 const onElementChange = () => {
   if (adRooms.value >= adCapacity.value) {
     adRooms.setCustomValidity('');
-    addErrorStyle(false, adRooms);
-    addErrorStyle(false, adCapacity);
-    disabledBtn(false);
   } else {
     adRooms.setCustomValidity('Комнат не хватает!');
-    addErrorStyle(true, adRooms);
-    addErrorStyle(true, adCapacity);
-    disabledBtn(true);
   }
 
   if (adRooms.value < adCapacity.value) {
     adRooms.setCustomValidity('Нужно больше комнат!');
-    addErrorStyle(true, adRooms);
-    addErrorStyle(true, adCapacity);
-    disabledBtn(true);
   } else {
     adRooms.setCustomValidity('');
-    addErrorStyle(false, adRooms);
-    addErrorStyle(false, adCapacity);
-    disabledBtn(false);
   }
 
-  if (adRooms.value === '100') {
-    adCapacity.value = adCapacity[3].value;
-    adCapacity[0].setAttribute('disabled', 'disabled');
-    adCapacity[1].setAttribute('disabled', 'disabled');
-    adCapacity[2].setAttribute('disabled', 'disabled');
-    adCapacity[3].removeAttribute('disabled', 'disabled');
-  } else {
-    adCapacity[0].removeAttribute('disabled', 'disabled');
-    adCapacity[1].removeAttribute('disabled', 'disabled');
-    adCapacity[2].removeAttribute('disabled', 'disabled');
-    adCapacity[3].setAttribute('disabled', 'disabled');
-  }
+  //---------------------------------------
+  /*
+  этот блок 146-159 находит нужные значения, но блокирует часть нужных
 
+  ROOMS_TO_CAPACITY[adRooms.value].forEach((item) => {
+    for (let i = 0; i < adCapacity.length; i++) {
+      console.log(`item = ${item}`);
+      console.log(`value = ${adCapacity[i]}`);
+
+      if (adCapacity[i].value === `${item}`) {
+        adCapacity[i].removeAttribute('disabled', 'disabled');
+        console.log(true);
+      } else {
+        adCapacity[i].setAttribute('disabled', 'disabled');
+        console.log(false);
+      }
+    }
+  });
+  */
+  //-----------------------------------------------------------------
   adRooms.reportValidity();
   adCapacity.reportValidity();
 };
+
+const switchGuestsCapacity = (rooms) => {
+  adCapacityOption.forEach((item) => {
+    //console.log(item.value); //значение option.value
+    //console.log(ROOMS_TO_CAPACITY[rooms]); //массив гостей
+    //console.log(rooms); //value выбранного значения
+    //console.log(ROOMS_TO_CAPACITY[rooms].includes(`${item.value}`));
+    //console.log(item.value === ROOMS_TO_CAPACITY[rooms][0]);
+
+    if (item.value === ROOMS_TO_CAPACITY[rooms][0]) {
+      item.selected = true;
+    }
+    item.disabled = !ROOMS_TO_CAPACITY[rooms].includes(`${item.value}`);
+  });
+};
+
+adRooms.addEventListener('change', (evt) => {
+  switchGuestsCapacity(evt.target.value);
+});
 
 adRooms.addEventListener('invalid', onElementInvalid);
 adRooms.addEventListener('change', onElementChange);
 adCapacity.addEventListener('invalid', onElementInvalid);
 adCapacity.addEventListener('change', onElementChange);
 
+//time in && time out
+adTimeIn.addEventListener('change', () => {
+  adTimeOut.value = adTimeIn.value;
+});
+
+adTimeOut.addEventListener('change', () => {
+  adTimeIn.value = adTimeOut.value;
+});
+
+//submit validity
+const onFormValidityCheck = (evt) => {
+  evt.preventDefault();
+
+  if (!adTitle.reportValidity()) {
+    adTitle.classList.add('ad-form__element-error');
+  }
+
+  if (!adPrice.reportValidity()) {
+    adPrice.classList.add('ad-form__element-error');
+  }
+
+};
+
+adForm.addEventListener('submit', onFormValidityCheck);
